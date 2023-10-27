1cd247df8648c0685075e5b13c6cbad835da7fbe
L19h7n1nG <AmadeusLightning@outlook.com>
2023-10-28 01:47:38 +0800

    (feat)brainfuck: .

--- brainfuck-mlir/braincc.cc
+++ brainfuck-mlir/braincc.cc
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
 
+#include "brainfuck/BrainfuckTypes.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/CommandLine.h"
@@ -19,8 +21,11 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Value.h"
+#include "mlir/IR/Verifier.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
 #include "mlir/Pass/Pass.h"
@@ -38,15 +43,69 @@ void regisrtyDialects(mlir::DialectRegistry& registry) {
 
 using namespace mlir::lightning::brainfuck;
 
+mlir::OwningOpRef<mlir::ModuleOp> parse_code(
+        mlir::MLIRContext& context,
+        std::string_view   code) {
+    mlir::OpBuilder builder(&context);
+
+    auto theModule = mlir::ModuleOp::create(builder.getUnknownLoc());
+
+    auto funcType = builder.getFunctionType(std::nullopt, std::nullopt);
+
+    // auto mainfunction = builder.create<mlir::func::FuncOp>(
+    //         builder.getUnknownLoc(), "main", funcType);
+
+    // theModule.push_back(mainfunction);
+
+    // auto entryBlock = mainfunction.addEntryBlock();
+
+    // builder.setInsertionPointToEnd(entryBlock);
+
+    // builder.create<mlir::func::ReturnOp>(builder.getUnknownLoc());
+    // builder.setInsertionPointToEnd(entryBlock);
+
+    for (auto&& ch : code) {
+        if (ch == '<') {
+            // builder.create<mlir::lightning::brainfuck::AddOp>(
+            //         builder.getUnknownLoc());
+            // auto lhsType = builder.create()
+            // mlir::Value lhs = mlir::Value::setType(builder.getI32Type());
+            mlir::Value lhs;
+            mlir::Value rhs;
+            auto        outType =
+                    mlir::lightning::brainfuck::PtrType::get(&context, 0);
+            // auto rhs =
+            auto op = builder.create<mlir::lightning::brainfuck::ShiftOp>(
+                    builder.getUnknownLoc(), outType, lhs, rhs);
+            // theModule.push_back(op);
+        }
+    }
+
+    if (failed(mlir::verify(theModule))) {
+        theModule.emitError("module verification error");
+        return nullptr;
+    }
+
+    return theModule;
+}
 
 int main(int argc, char** argv) {
-    // mlir::registerAllPasses();
+    auto s =
+            "++++++++++++++++++++++++>>>>>>>>>>>>>++++++++++++++++<<<<<<<<<<<<+++++++-------";
+    mlir::MLIRContext context;
+    context.loadDialect<
+            mlir::func::FuncDialect,
+            mlir::lightning::brainfuck::BrainfuckDialect>();
 
-    // mlir::lightning::brainfuck
-    mlir::DialectRegistry registry;
-    regisrtyDialects<
-            mlir::lightning::brainfuck::BrainfuckDialect, mlir::func::FuncDialect>(registry);
+    mlir::OwningOpRef<mlir::ModuleOp> module = parse_code(context, s);
 
-    return mlir::asMainReturnCode(mlir::MlirOptMain(
-            argc, argv, "Brainfuck optimizer driver\n", registry));
+    module->dump();
+
+    // mlir::DialectRegistry registry;
+    // regisrtyDialects<
+    //         mlir::lightning::brainfuck::BrainfuckDialect,
+    //         mlir::func::FuncDialect>(registry);
+
+    // return mlir::asMainReturnCode(mlir::MlirOptMain(
+    //         argc, argv, "Brainfuck optimizer driver\n", registry));
 }
--- brainfuck-mlir/test/add.mlir
+++ brainfuck-mlir/test/add.mlir
@@ -1,6 +1,16 @@
 module {
-  func.func @main(%arg0: !brainfuck.cell) -> !brainfuck.cell {
-      
+  func.func @test_ptr(%arg0: !brainfuck.ptr, %arg1: i32) -> i32 {
+
+    %1 = brainfuck.shr %arg0, %arg1: (!brainfuck.ptr, i32) -> !brainfuck.ptr
+
+    return %arg1: i32
+  } 
+
+  func.func @test_cell(%arg0: !brainfuck.cell, %arg1: i32) -> !brainfuck.cell {
+    
+    
+      %1 = brainfuck.add %arg0, %arg1 : (!brainfuck.cell, i32) -> !brainfuck.cell
+      %2 = brainfuck.add %1, %arg1 : (!brainfuck.cell, i32) -> !brainfuck.cell
 
     return %arg0 : !brainfuck.cell
   }
